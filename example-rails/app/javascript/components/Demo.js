8a22409276f52567b7434b7215d0f639f9847245
Michael Yin <michael@gloryleague.com>
2023-10-28 16:06:00 +1300

    importmap: replace with mui

--- example-rails/app/javascript/components/Demo.js
+++ example-rails/app/javascript/components/Demo.js
@@ -1,71 +1,77 @@
-import {
-  Button,
-  Autocomplete,
-  ButtonGroup,
-  Stack,
-  List,
-  ListItem,
-  ListItemButton,
-  Sheet,
-  Typography,
-  CardContent,
-  AspectRatio,
-  Card,
-} from "@mui/joy";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import Autocomplete from "@mui/material/Autocomplete";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import ListItemText from "@mui/material/ListItemText";
+
 import { html } from "htm/react";
 
 export default function Demo() {
   return html`
     <${Stack} spacing=${2} style=${{ margin: 10 }}>
-      <${Button} variant="solid">Hello world<//>
+      <${Button} variant="contained">Hello world<//>
       <${ButtonGroup}>
-        <${Button} color="primary">One<//>
+        <${Button} color="secondary">One<//>
         <${Button}>Two<//>
         <${Button}>Three<//>
       <//>
-      <${Autocomplete} options=${["Option 1", "Option 2"]} />
-      <${Sheet} variant="outlined">
+      <${Autocomplete}
+        options=${["Option 1", "Option 2"]}
+        renderInput=${(params) =>
+          html`<${TextField} ...${params} label="Combo box" />`}
+      />
+      <${Paper}>
         <${List}>
-          <${ListItem}>
-            <${ListItemButton}>Home<//>
+          <${ListItem} disablePadding>
+            <${ListItemButton}>
+              <${ListItemText} primary="Home" />
+            <//>
           <//>
-          <${ListItem}>
-            <${ListItemButton}>Projects<//>
+          <${ListItem} disablePadding>
+            <${ListItemButton}>
+              <${ListItemText} primary="About" />
+            <//>
           <//>
-          <${ListItem}>
-            <${ListItemButton}>Settings<//>
+          <${ListItem} disablePadding>
+            <${ListItemButton}>
+              <${ListItemText} primary="Contact" />
+            <//>
           <//>
         <//>
       <//>
       <${Card} sx=${{ width: 320 }}>
-        <div>
-          <${Typography} level="title-lg">Yosemite National Park<//>
-          <${Typography} level="body-sm">April 24 to May 02, 2021<//>
-        </div>
-        <${AspectRatio} minHeight="120px" maxHeight="200px">
-          <img
-            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
-            srcset="
-              https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x
-            "
-            loading="lazy"
-            alt=""
-          />
-        <//>
-        <${CardContent} orientation="horizontal">
+        <${CardHeader}
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
+          srcset="
+            https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x
+          "
+          loading="lazy"
+          alt=""
+          minHeight="120px"
+          maxHeight="200px"
+        />
+        <${CardContent}>
           <div>
             <${Typography} level="body-xs">Total price:<//>
             <${Typography} fontSize="lg" fontWeight="lg"> $2,900 <//>
           </div>
-          <${Button}
-            variant="solid"
-            size="md"
-            color="primary"
-            aria-label="Explore Bahamas Islands"
-            sx=${{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-          >
-            Explore
-          <//>
+        <//>
+        <${CardActions}>
+          <${Button} variant="contained"> Explore <//>
         <//>
       <//>
     <//>
